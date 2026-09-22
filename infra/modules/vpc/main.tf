d4ce6ce309b806089601bc09bf9ae37888d8d930
imad <j166a775433@gmail.com>
2026-09-22 20:45:43 +0100

    feat(terraform): add private subnet resource
    Refs #67

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -28,3 +28,16 @@ resource "aws_subnet" "public" {
     Name = "${var.name}-public-${count.index + 1}"
   }
 }
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name}-public-${count.index + 1}"
+  }
+}
