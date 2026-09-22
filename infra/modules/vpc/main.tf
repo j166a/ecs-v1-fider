40387bb8f1ecfa0c3f540e4f741487c6d8ad4d7c
imad <j166a775433@gmail.com>
2026-09-22 20:43:08 +0100

    feat(terraform): add public subnet resource
    Refs #67

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -15,3 +15,16 @@ resource "aws_internet_gateway" "this" {
     Name = "${var.name}-igw"
   }
 }
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name}-public-${count.index + 1}"
+  }
+}
