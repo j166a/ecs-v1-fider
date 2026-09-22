2eb85b5aa952d0f41822ed3e863c1ff446ab0395
imad <j166a775433@gmail.com>
2026-09-22 19:50:49 +0100

    feat(terraform): define VPC module variables
    Refs #67

---
+++ infra/modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  description = "Name prefix for VPC resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "Availability zones used by the subnets"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
