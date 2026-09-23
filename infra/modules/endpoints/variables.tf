4d85f35ed28ba3ec51a455069c0dec783fe1393e
imad <j166a775433@gmail.com>
2026-09-23 12:17:29 +0100

    feat(terraform): define VPC endpoint module inputs
    Define networking, security and tagging inputs required by gateway and interface endpoints.
    
    Refs #73

---
+++ infra/modules/endpoints/variables.tf
@@ -0,0 +1,25 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "IDs of private subnets"
+  type        = list(string)
+}
+
+variable "private_route_table_ids" {
+  description = "IDs of private route tables associated with private subnets"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "ID of security group attached to interface VPC endpoints"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags applied to endpoint resources"
+  type        = map(string)
+  default     = {}
+}
