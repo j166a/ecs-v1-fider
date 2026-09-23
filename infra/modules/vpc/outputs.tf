172f54c75de54f8999393c12373b92c42ee849ff
imad <j166a775433@gmail.com>
2026-09-23 11:33:49 +0100

    feat(terraform): expose private route table IDs
    Expose the private route table for the S3 gateway endpoint association.
    
    Refs #73

--- infra/modules/vpc/outputs.tf
+++ infra/modules/vpc/outputs.tf
@@ -12,3 +12,8 @@ output "private_subnet_ids" {
   description = "IDs of private subnets"
   value       = aws_subnet.private[*].id
 }
+
+output "private_route_table_ids" {
+  description = "IDs of private route tables"
+  value       = [aws_route_table.private.id]
+}
