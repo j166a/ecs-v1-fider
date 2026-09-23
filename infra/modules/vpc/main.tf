a5331adbdbbc302875cd4bb0abdcb00a5c054e8b
imad <j166a775433@gmail.com>
2026-09-23 16:49:47 +0100

    fix(terraform): disable public IPs on private subnets

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -44,7 +44,7 @@ resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.private_subnet_cidrs[count.index]
   availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = merge(
     var.tags,
