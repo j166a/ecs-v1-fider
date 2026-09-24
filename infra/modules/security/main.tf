c3ca23d7fab7b830b182a568c476f1ff235736d5
imad <j166a775433@gmail.com>
2026-09-24 16:40:08 +0100

    feature(terraform): add ALB security group rules
    Allow HTTP and HTTPS ingress from the internet and restrict ALB egress to ECS tasks on port 3000.
    
    Refs #80

--- infra/modules/security/main.tf
+++ infra/modules/security/main.tf
@@ -11,6 +11,32 @@ resource "aws_security_group" "alb" {
   )
 }
 
+resource "aws_vpc_security_group_ingress_rule" "alb_http" {
+  security_group_id = aws_security_group.alb.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+resource "aws_vpc_security_group_ingress_rule" "alb_https" {
+  security_group_id = aws_security_group.alb.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_to_ecs" {
+  security_group_id = aws_security_group.alb.id
+
+  referenced_security_group_id = aws_security_group.ecs.id
+  from_port                    = 3000
+  ip_protocol                  = "tcp"
+  to_port                      = 3000
+}
+
 resource "aws_security_group" "ecs" {
   name        = "${var.name}-ecs-sg"
   description = "Security group for ECS tasks"
