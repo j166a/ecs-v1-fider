be7c51f0c3913f5c7435f1218912c95c5e6c8a3e
imad <j166a775433@gmail.com>
2026-09-24 11:31:55 +0100

    feat(terraform): bootstrap remote state bucket
    Create the S3 backend bucket with versioning, encryption, public access blocking, and an output for the bucket name.
    
    Refs #197

--- infra/bootstrap/variables.tf
+++ infra/bootstrap/variables.tf
@@ -3,3 +3,8 @@ variable "aws_region" {
   type        = string
   default     = "eu-west-2"
 }
+
+variable "state_bucket_name" {
+  description = "Globally unique S3 bucket name for Terraform remote state"
+  type        = string
+}
