d355f5038004a265d91efbaa364b5f2e632e51dd
imad <j166a775433@gmail.com>
2026-09-24 19:25:09 +0100

    feature(terraform): allow ECS access to interface endpoints
    Permit HTTPS traffic from the ECS task security group to the interface endpoints.
    
    Refs #80

--- infra/modules/security/main.tf
+++ infra/modules/security/main.tf
@@ -99,6 +99,15 @@ resource "aws_vpc_security_group_ingress_rule" "rds_from_ecs" {
   to_port                      = 5432
 }
 
+resource "aws_vpc_security_group_ingress_rule" "endpoints_from_ecs" {
+  security_group_id = aws_security_group.endpoints.id
+
+  referenced_security_group_id = aws_security_group.ecs.id
+  from_port                    = 443
+  ip_protocol                  = "tcp"
+  to_port                      = 443
+}
+
 resource "aws_security_group" "endpoints" {
   name        = "${var.name}-endpoints-sg"
   description = "Security group for VPC interface endpoints"
